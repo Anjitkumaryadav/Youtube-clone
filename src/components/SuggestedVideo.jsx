dd55e88e9c8b397f52edece7787e0a772f38ec3c
Anjitkumaryadav <anjitaniket@gmail.com>
2024-11-17 16:44:49 +0530

    api renew

--- src/components/SuggestedVideo.jsx
+++ src/components/SuggestedVideo.jsx
@@ -7,10 +7,10 @@ import { abbreviateNumber } from "js-abbreviation-number";
 function SuggestedVideo({ video }) {
   console.log(video);
   return (
-    <div>
+    <div className="">
       <Link to={`/video/${video?.videoId}`}>
         <div className="flex mb-3 ">
-          <div className="relative h-24 lg:h-20 xl:h-24 w-40 min-w-[168px] lg:w-32 lg:min-w-[128px] xl:w-40 xl:min-w-[168px] rounded-xl hover:rounded-none duration-200">
+          <div className="relative h-24 lg:h-20 xl:h-24 w-40 min-w-[168px] lg:w-32 lg:min-w-[100px] xl:w-20 xl:min-w-[168px] rounded-xl hover:rounded-none duration-200">
             <img
               className="h-full w-full rounded-lg"
               src={video?.thumbnails[0]?.url}
@@ -18,7 +18,7 @@ function SuggestedVideo({ video }) {
             {video?.lengthSeconds && <Time time={video?.lengthSeconds} />}
           </div>
           <div className="flex flex-col ml-3 overflow-hidden">
-            <span className="text-sm lg:text-xs xl:text-sm font-bold line-clamp-2 ">
+            <span className="text-sm lg:text-xs xl:text-[11px] font-bold line-clamp-2 ">
               {video?.title}
             </span>
             <span className="text-[12px] lg:text-[10px] xl:text-[12px] font-semibold mt-2  flex items-center">
